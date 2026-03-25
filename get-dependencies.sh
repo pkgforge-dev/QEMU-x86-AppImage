db51cfe489e551c6a09275b9f75456644327286e
Link4Electronics <link4electronics@gmail.com>
2026-03-25 13:01:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,6 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm      \
-    edk2-ovmf                \
     gtk3                     \
     libdecor                 \
     qemu-audio-pa            \
@@ -15,8 +14,7 @@ pacman -Syu --noconfirm      \
     qemu-system-x86          \
     qemu-system-x86-firmware \
     qemu-ui-gtk              \
-    sdl2                     \
-    seabios
+    sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -24,6 +22,11 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
+if [ "$ARCH" = "aarch64" ]; then
+    wget https://umea.mirror.pkgbuild.com/extra/os/x86_64/edk2-ovmf-202508-1-any.pkg.tar.zst
+    wget https://umea.mirror.pkgbuild.com/extra/os/x86_64/seabios-1.17.0-2-any.pkg.tar.zst
+    pacman -U seabios-*.pkg.tar.zst edk2-ovmf-*.pkg.tar.zst
+fi
 
 # If the application needs to be manually built that has to be done down here
 #mkdir -p ./AppDir/bin
