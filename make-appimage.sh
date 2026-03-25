d01b6a7357d143f64a10c99ad79b863e8736af42
Link4Electronics <link4electronics@gmail.com>
2026-03-25 08:58:06 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,6 +11,9 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=https://gitlab.com/qemu-project/qemu/-/raw/master/ui/icons/qemu.svg
 export DESKTOP=DUMMY
 export MAINBIN=qemu-syste-x86_64
+export DEPLOY_GTK=1
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/qemu-system-x86_64 /usr/bin/qemu-system-i386
